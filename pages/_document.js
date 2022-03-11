4cc324ff73f2befcaf02668f2fe85ac1b4e16450
indigo <indigonakamoto@gmail.com>
2022-03-11 13:56:01 +0800

    Do not optimize fonts.

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+//TODO: Download fonts locally and update source. 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Jura:wght@300;500&family=Poppins:ital,wght@0,200;0,400;1,200;1,400&display=swap" rel="stylesheet" />
       </Head>
       <body>
