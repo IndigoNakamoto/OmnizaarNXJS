b509e99e66732e03506e9ac4bef048c2d32bc706
indigo <indigonakamoto@gmail.com>
2022-03-11 10:32:13 +0800

    About - Get data from strapi. Update style.

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,38 @@
 import Head from 'next/head'
+import Navigation from '../components/navigation'
+import { motion } from 'framer-motion'
+import Article from '../components/article';
+import Hero from '../components/articleHero'
 
-export default function About() {
+About.getInitialProps = async (ctx) => {
+    const res = await fetch("https://test-deploy-digital-ocean-hhbor.ondigitalocean.app/api/about?populate=*")
+    const json = await res.json()
+    return { aboutData: json.data}
+}
+
+//TODO: Include style colors in Strapi for editor to choose. 
+//TODO: Pass down style colors. 
+export default function About({aboutData}) {
+    const body = aboutData.attributes.Body
+    const hero = aboutData.attributes.Hero.data.attributes.formats.large.url
+    const background = '#212c37'
+    const accent1 = 'green'
+    const accent2 = '#37aec7'
+    const fontcolor = '#fcfcfc'
+
     return (
-        <>
-            <Head>
-                <title>Omnizaar - About Us</title>
-            </Head>
-            <h1>About</h1>
-        </>
+        <motion.div initial={{opacity:0.75}} animate={{opacity:1}}>
+            <>
+                <Head>
+                    <title>Omnizaar - About Us</title>
+                </Head>
+                
+                <Navigation accent1={accent1}/> 
+                <Hero urlHero={hero} />
+                    <Article content={body} title={'ABOUT'} 
+                    accent1={accent1} subtitle={'About Omnizaar'}
+                    />                
+            </>
+      </motion.div>
     )
-  }
+}
