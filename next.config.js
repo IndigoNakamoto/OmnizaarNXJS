db6ebc002797d1f5ab0217fb1dabb9d433ef73ed
indigo <indigonakamoto@gmail.com>
2022-03-11 11:33:07 +0800

    Config style components

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    reactStrictMode: true,
+    images: {
+      loader: "default",
+      domains: ["localhost"],
+    },
+    compiler: {
+      // ssr and displayName are configured by default
+      styledComponents: true,
+    },
+  };
