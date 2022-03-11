c7da2febe74486f7744acfa3299f8a6e05dd3917
indigo <indigonakamoto@gmail.com>
2022-03-11 10:26:20 +0800

    Update global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/global.css'
-// can be used to keep state when navigating between pages
-
-
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
+import "../styles/globals.css";
+import Head from "next/head";
+export default function MyApp({ Component, pageProps }) {
+return (
+<>
+  <Head>
+   {/* This ways to add css on global website use local asset folder withhtml link tag */}
+        
+ </Head>
+  <Component {...pageProps} />
+</>
+)}
